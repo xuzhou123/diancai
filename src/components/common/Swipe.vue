6c4990cf723c3dfa6aad77a7b8d98766996e09dd
zhouxu <895118827@qq.com>
2019-06-29 00:33:47 +0800

    init tabbar swipe

---
+++ src/components/common/Swipe.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="swipe">
+      <mt-swipe :auto="4000">
+        <mt-swipe-item><img src="../../assets/swipe/swipe1.jpg" alt=""></mt-swipe-item>
+        <mt-swipe-item><img src="../../assets/swipe/swipe2.jpg" alt=""></mt-swipe-item>
+        <mt-swipe-item><img src="../../assets/swipe/swipe3.jpg" alt=""></mt-swipe-item>
+        <mt-swipe-item><img src="../../assets/swipe/swipe4.jpg" alt=""></mt-swipe-item>
+      </mt-swipe>
+    </div>
+</template>
+
+<script>
+import { Swipe, SwipeItem } from 'mint-ui'
+export default {
+  name: 'Swipe',
+  components: {
+    'mt-swipe': Swipe,
+    'mt-swipe-item': SwipeItem
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.swipe {
+  height: 150px;
+  img {
+    height: 150px;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/common/Tabbar1.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="tabbar">
+
+      <mt-tab-container v-model="active" swipeable>
+        <mt-tab-container-item id="home1">
+          <div style="height:100px;background:#ccc;"></div>
+        </mt-tab-container-item>
+        <mt-tab-container-item id="play1">
+          <div style="height:100px;background:red;"></div>
+        </mt-tab-container-item>
+        <mt-tab-container-item id="order1">
+          <div style="height:100px;background:green;"></div>
+        </mt-tab-container-item>
+      </mt-tab-container>
+
+      <mt-tabbar  v-model="selected">
+        <mt-tab-item id="home">
+          <img slot="icon" src="../../assets/common/home.png" alt="">
+          首页
+        </mt-tab-item>
+        <mt-tab-item id="play">
+          <img slot="icon" src="../../assets/common/order.png" alt="">
+          娱乐
+        </mt-tab-item>
+        <mt-tab-item id="order">
+          <img slot="icon" src="../../assets/common/index.png" alt="">
+          订单
+        </mt-tab-item>
+        <mt-tab-item id="index">
+          <img slot="icon" src="../../assets/common/index.png" alt="">
+          我的
+        </mt-tab-item>
+      </mt-tabbar>
+    </div>
+</template>
+
+<script>
+import { TabContainer, TabContainerItem, Tabbar, TabItem } from 'mint-ui'
+export default {
+  name: 'Tabbar',
+  components: {
+    'mt-tab-container': TabContainer,
+    'mt-tab-container-item': TabContainerItem,
+    'mt-tabbar': Tabbar,
+    'mt-tab-item': TabItem
+  },
+  data () {
+    return {
+      active: 'home1',
+      selected: 'home'
+    }
+  },
+  watch: {
+    active (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    },
+    selected (newVal, oldVal) {
+      this.active = newVal + '1'
+      console.log(newVal, oldVal)
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
